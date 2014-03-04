ab2b45bf6ebd4b5958a0dbc90666703a63571312
daiz <da.iz.mg@gmail.com>
2014-03-05 00:36:20 +0900

    first commit.

---
+++ background.js
@@ -0,0 +1,19 @@
+﻿// background.js
+(function() {
+    if(!localStorage.scroll) {
+        localStorage.scroll = "on";
+    }
+})();
+
+function getMessages(request, sender, callback) {
+   if (request.action == "checked") {
+      callback(localStorage.scroll);
+   }else if(request.action = "setting") {
+      localStorage.scroll = request.value;
+   }
+}
+   
+chrome.extension.onRequest.addListener(getMessages);
+
+
+
---
+++ popup.js
@@ -0,0 +1,22 @@
+﻿function settings() {
+    var v;
+    if(document.getElementById("scroll").checked) {
+        v = "on"
+    }else {
+        v = "off"
+    }
+    chrome.extension.sendRequest({action: "setting", "value": v}, function(){});
+}
+(function() {
+    chrome.extension.sendRequest({action: "checked"}, function(c) {
+       if(c == "on") {
+          document.getElementById("scroll").checked = true;
+          console.log("on");
+       }else {
+          document.getElementById("scroll").checked = false;
+          console.log("off");
+       }
+    });
+})();
+
+document.getElementById("scroll").addEventListener("click", settings, false);
